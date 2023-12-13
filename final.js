1d54baf69e192ae3ddf3887b4aa6cacc5a9eb9e1
zakiarifa04 <zakia.rifa04@gmail.com>
2023-12-13 12:01:48 +0600

    7th

--- final.js
+++ final.js
@@ -1,13 +1,14 @@
 function connect(){
     var searchTerm = document.getElementById("searchBox").value ;
 
-    var url = `https://openweathermap.org/api/one-call-3${searchTerm}&appid={0aaf660f76d0960bde8633f5ae05d775}`
+    var url = (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0aaf660f76d0960bde8633f5ae05d775
+    `)
     console.log (url)
 
     fetch (url)
     .then (res => res.json())
     .then (data => displayWeatherData(data))
-    .catch (error => console.log('Error;', error()) ;
+    .catch (error => console.log('Error;', error())) ;
 
 }
 function displayWeatherData(data) {
