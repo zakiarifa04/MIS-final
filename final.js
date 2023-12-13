d643329a60c1127fdbd0f72af8de8b2dd68714cb
zakiarifa04 <zakia.rifa04@gmail.com>
2023-12-13 11:25:54 +0600

    6th

--- final.js
+++ final.js
@@ -7,9 +7,27 @@ function connect(){
     fetch (url)
     .then (res => res.json())
     .then (data => displayWeatherData(data))
-    
+    .catch (error => console.log('Error;', error()) ;
 
-
-
-
+}
+function displayWeatherData(data) {
+    const weather = {
+        temperature: data.main.temp - 273.15, // Kelvin to Celsius
+        humidity: data.main.humidity,
+        pressure: data.main.pressure,
+        weatherIcon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+        description: data.weather[0].description,
+        city: data.name,
+        country: data.sys.country
+    };
+    const dataItem = document.createElement('div');
+    dataItem.classList.add('data-item');
+    dataItem.innerHTML = `
+        <h2>${weather.city}, ${weather.country}</h2>
+        <img src="${weather.weatherIcon}" alt="${weather.description}">
+        <p><strong>Temperature:</strong> ${weather.temperature.toFixed(1)} &#8451;</p>
+        <p><strong>Humidity:</strong> ${weather.humidity}%</p>
+        <p><strong>Pressure:</strong> ${weather.pressure} hPa</p>
+    `;
+    weatherData.appendChild(dataItem);
 }
