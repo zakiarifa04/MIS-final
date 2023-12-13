b42e37320b1719326ca9345dbe4fc83d65b35794
zakiarifa04 <zakia.rifa04@gmail.com>
2023-12-13 11:10:24 +0600

    5th

--- final.js
+++ final.js
@@ -3,6 +3,10 @@ function connect(){
 
     var url = `https://openweathermap.org/api/one-call-3${searchTerm}&appid={0aaf660f76d0960bde8633f5ae05d775}`
     console.log (url)
+
+    fetch (url)
+    .then (res => res.json())
+    .then (data => displayWeatherData(data))
     
 
 
